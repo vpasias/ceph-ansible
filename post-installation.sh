cf0a13e03ddc334be98ef3a56848292ee049adf2
vpasias <52623837+vpasias@users.noreply.github.com>
2021-03-22 13:52:40 +0200

    Update post-installation.sh

--- post-installation.sh
+++ post-installation.sh
@@ -1,3 +1,9 @@
+#! /bin/sh
+
+export LC_ALL=C
+export LC_CTYPE="UTF-8",
+export LANG="en_US.UTF-8"
+
 #docker exec -it ceph-mon-server701 ceph -s
 # http://bwdt.breqwatr.com/
 # https://docs.ceph.com/en/latest/rbd/rbd-openstack/
@@ -19,7 +25,21 @@ ceph osd pool create vms
 
 rbd pool init volumes
 rbd pool init images
-
-ceph auth ls
 rbd pool init backups
 rbd pool init vms
+
+ceph auth ls
+
+ceph auth get-or-create client.glance mon 'profile rbd' osd 'profile rbd pool=images' mgr 'profile rbd pool=images'
+ceph auth get-or-create client.cinder mon 'profile rbd' osd 'profile rbd pool=volumes, profile rbd pool=vms, profile rbd-read-only pool=images' mgr 'profile rbd pool=volumes, profile rbd pool=vms'
+ceph auth get-or-create client.nova mon 'profile rbd' osd 'profile rbd pool=volumes, profile rbd pool=vms, profile rbd-read-only pool=images' mgr 'profile rbd pool=volumes, profile rbd pool=vms'
+ceph auth get-or-create client.cinder-backup mon 'profile rbd' osd 'profile rbd pool=backups' mgr 'profile rbd pool=backups'
+
+ceph auth get-or-create client.glance | ssh server104 'sudo tee /etc/ceph/ceph.client.glance.keyring'
+ssh server104 'sudo chown glance:glance /etc/ceph/ceph.client.glance.keyring'
+ceph auth get-or-create client.cinder | ssh server104 'sudo tee /etc/ceph/ceph.client.cinder.keyring'
+ssh server104 'sudo chown cinder:cinder /etc/ceph/ceph.client.cinder.keyring'
+ceph auth get-or-create client.cinder-backup | ssh server104 'sudo tee /etc/ceph/ceph.client.cinder-backup.keyring'
+ssh server104 'sudo chown cinder:cinder /etc/ceph/ceph.client.cinder-backup.keyring'
+
+ceph auth get client.cinder > ceph.client.cinder.keyring
